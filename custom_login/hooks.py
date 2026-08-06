0e44e3ebb3fbdd49e8695db97fa4c3221a46f1e4
Morphocharm28 <Thomas@morphosistec.com>
2026-08-06 10:52:26 +0530

    changes in hooks.py

--- custom_login/hooks.py
+++ custom_login/hooks.py
@@ -7,7 +7,7 @@ app_license = "mit"
 
 after_install = "custom_login.custom_login.install.after_install"
 app_include_css = [
-    "/assets/custom_login/css/variables.css",
+    # "/assets/custom_login/css/variables.css",
     # "/assets/custom_login/css/navbar.css",
       "/assets/custom_login/css/sidebar.css",
       "/assets/custom_login/css/morphosis_theme.css",
